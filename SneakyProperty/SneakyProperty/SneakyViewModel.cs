a95859f92a034fe78d676910c66e696a664f25cd
Juli Racca <juliracca@gmail.com>
2016-04-21 10:25:33 -0300

    add base project

---
+++ SneakyProperty/SneakyProperty/SneakyViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ReactiveUI;
+
+namespace SneakyProperty
+{
+	public class SneakyViewModel : ReactiveObject
+	{
+		private bool _IsSneaky;
+
+		public bool IsSneaky
+		{
+			get { return _IsSneaky; }
+			set { this.RaiseAndSetIfChanged(ref _IsSneaky, value); }
+		}
+
+		public SneakyViewModel ()
+		{
+		}
+	}
+}
